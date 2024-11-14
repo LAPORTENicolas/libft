1d232b21960507f874448fddf25eaf341487bb91
laportenicolas <laportenicolas@protonmail.com>
2024-11-14 21:09:06 +0100

    remove

---
+++ libft/srcs/ft_lstlast_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaporte <nlaporte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 00:50:21 by nlaporte          #+#    #+#             */
+/*   Updated: 2024/11/11 00:50:22 by nlaporte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	if (lst->next != NULL)
+		return (ft_lstlast(lst->next));
+	return (lst);
+}
---
+++ libft/srcs/ft_lstmap_bonus.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlaporte <nlaporte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 00:20:12 by nlaporte          #+#    #+#             */
+/*   Updated: 2024/11/12 00:20:14 by nlaporte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+void	ft_cpy(t_list *lst, t_list *new, void *(*f)(void *))
+{
+	if (lst == NULL && new != NULL && f != NULL)
+		return ;
+	ft_lstadd_back(&new, ft_lstnew(f(lst->content)));
+	ft_cpy(lst->next, new, f);
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	new = ft_lstnew(f(lst->content));
+	ft_cpy(lst->next, new, f);
+	ft_lstclear(&lst, del);
+	return (new);
+}

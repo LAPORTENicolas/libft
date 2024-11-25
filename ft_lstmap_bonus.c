7a8453ea090fafcfe974219215a7f82473278221
laportenicolas <laportenicolas@protonmail.com>
2024-11-25 06:47:49 +0100

    fix

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -11,22 +11,39 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
-void	ft_cpy(t_list *lst, t_list *new, void *(*f)(void *))
+void	ft_abort(t_list *lst, void *last, void (*del)(void *))
 {
-	if (lst == NULL)
-		return ;
-	ft_lstadd_back(&new, ft_lstnew(f(lst->content)));
-	ft_cpy(lst->next, new, f);
+	ft_lstclear(&lst, del);
+	del(last);
 }
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new;
+	t_list	*new_l;
+	t_list	*new_n;
+	void	*tmp;
 
 	if (lst == NULL || f == NULL || del == NULL)
 		return (NULL);
-	new = ft_lstnew(f(lst->content));
-	ft_cpy(lst->next, new, f);
-	return (new);
+	new_l = NULL;
+	while (lst != NULL)
+	{
+		tmp = f(lst->content);
+		if (tmp == NULL)
+		{
+			ft_lstclear(&new_l, del);
+			return (NULL);
+		}
+		new_n = ft_lstnew(tmp);
+		if (new_n == NULL)
+		{
+			ft_abort(new_l, tmp, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_l, new_n);
+		lst = lst->next;
+	}
+	return (new_l);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -26,6 +26,8 @@ static size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
+	if (s == NULL)
+		return (0);
 	while (s[i])
 		i++;
 	return (i);
@@ -34,15 +36,13 @@ static size_t	ft_strlen(const char *s)
 static char	*ft_strndup(const char *s, size_t n)
 {
 	size_t	i;
-	size_t	length;
 	char	*res;
 
-	length = ft_strlen(s);
-	res = malloc(sizeof(char) * (length + 1));
+	res = malloc(sizeof(char) * (n + 1));
 	if (res == NULL)
 		return (NULL);
 	i = 0;
-	while (i < length && i < n)
+	while (i < n)
 	{
 		res[i] = s[i];
 		i++;
@@ -58,11 +58,31 @@ char	*ft_strtrim(const char *s, const char *set)
 
 	i = 0;
 	if (s == NULL || set == NULL)
-		return (NULL);
+		return (ft_strndup("", 0));
 	while (s[i] && is_inside(s[i], set) == 1)
 		i++;
+	if (i == ft_strlen(s))
+		return (ft_strndup("", 0));
 	s_s = ft_strlen(&s[i]);
 	while (is_inside(s[s_s + i - 1], set) == 1)
 		s_s--;
 	return (ft_strndup(&s[i], s_s));
 }
+/*
+MKO 24: mcheck(s, strlen("tripouille") + 1); free(s); showLeaks();
+MKO 28: mcheck(s, strlen("tripouille") + 1); free(s); showLeaks();
+MKO 48: mcheck(s, 2); free(s); showLeaks();
+*/
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("test strtrim\n");
+	char *s;
+
+	s = ft_strtrim("   xxx   xxx", " x");
+	printf("res %li\n", ft_strlen(s));
+	printf("res %li\n", ft_strlen("tripouille"));
+	free(s);
+	return (0);
+}*/
